cc82deb62d515b4dcc7853aafa1b9f0594f20c63
unknown <chenyuanfei@genius.com>
2025-03-26 17:56:46 +0800

    Signed-off-by: unknown <chenyuanfei@genius.com>

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,15 +2,16 @@
 import type { FC } from 'react'
 import React from 'react'
 import {
-  Bars3Icon, // 导航菜单图标
-  PencilSquareIcon, // 编辑图标
+  Bars3Icon,
+  PencilSquareIcon,
+  ArrowDownTrayIcon
 } from '@heroicons/react/24/solid'
 import AppIcon from '@/app/components/base/app-icon'
 
 <Button
   className="ml-2"
   onClick={() => handleExportConversation()}
-  icon={<ExportIcon className="w-4 h-4" />}
+  icon={<ArrowDownTrayIcon className="w-4 h-4" />}
 >
   <span>{t('app.chat.export')}</span>
 </Button>
