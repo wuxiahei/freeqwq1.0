9c15695343fa2a2fbf296a05d69c0377926d1164
unknown <chenyuanfei@genius.com>
2025-03-27 14:13:44 +0800

    Signed-off-by: unknown

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -7,6 +7,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChatBubbleOvalLeftEllipsisIcon as ChatBubbleOvalLeftEllipsisSolidIcon } from '@heroicons/react/24/solid'
 import Button from '@/app/components/base/button'
+import { useConversation } from '@/hooks/useConversation'
 // import Card from './card'
 import type { ConversationItem } from '@/types/app'
 
@@ -30,17 +31,42 @@ const Sidebar: FC<ISidebarProps> = ({
   list,
 }) => {
   const { t } = useTranslation()
+  const { setCurrConversationId, exportConversationToPDF } = useConversation()
+
+  const handleNewConversation = () => {
+    const timestamp = new Date().getTime()
+    setCurrConversationId(`new-${timestamp}`)
+    onCurrentIdChange(`new-${timestamp}`)
+  }
+
+  const handleExportPDF = () => {
+    if (currentId && currentId !== '-1')
+      exportConversationToPDF(currentId)
+  }
+
+  const handleNewConversation = () => {
+    setCurrConversationId('-1')
+    onCurrentIdChange('-1')
+  }
+
   return (
     <div
       className="shrink-0 flex flex-col overflow-y-auto bg-white pc:w-[244px] tablet:w-[192px] mobile:w-[240px]  border-r border-gray-200 tablet:h-[calc(100vh_-_3rem)] mobile:h-screen"
     >
       {list.length < MAX_CONVERSATION_LENTH && (
-        <div className="flex flex-shrink-0 p-4 !pb-0">
+        <div className="flex flex-shrink-0 p-4 !pb-0 gap-2">
           <Button
-            onClick={() => { onCurrentIdChange('-1') }}
+            onClick={handleNewConversation}
             className="group block w-full flex-shrink-0 !justify-start !h-9 text-primary-600 items-center text-sm">
             <PencilSquareIcon className="mr-2 h-4 w-4" /> {t('app.chat.newChat')}
           </Button>
+          {currentId && currentId !== '-1' && (
+            <Button
+              onClick={handleExportPDF}
+              className="group block w-full flex-shrink-0 !justify-start !h-9 text-primary-600 items-center text-sm">
+              {t('app.chat.exportPDF')}
+            </Button>
+          )}
         </div>
       )}
 
@@ -74,9 +100,6 @@ const Sidebar: FC<ISidebarProps> = ({
           )
         })}
       </nav>
-      {/* <a className="flex flex-shrink-0 p-4" href="https://langgenius.ai/" target="_blank">
-        <Card><div className="flex flex-row items-center"><ChatBubbleOvalLeftEllipsisSolidIcon className="text-primary-600 h-6 w-6 mr-2" /><span>LangGenius</span></div></Card>
-      </a> */}
       <div className="flex flex-shrink-0 pr-4 pb-4 pl-4">
         <div className="text-gray-400 font-normal text-xs">© {copyRight} {(new Date()).getFullYear()}</div>
       </div>
