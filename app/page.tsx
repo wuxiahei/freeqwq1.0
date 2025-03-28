6de04af51ce03d1b6db8fbcdd8d09ee2ee7ec4c3
unknown <chenyuanfei@genius.com>
2025-03-28 10:20:11 +0800

    Signed-off-by: 8

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import type { FC } from 'react'
 import React from 'react'
 import Main from '@/app/components'
-import GlobeError from '@/app/components/custom/error'
 
 interface HomeProps {
   searchParams: { userName?: string; token?: string }
@@ -9,14 +8,6 @@ interface HomeProps {
 
 const Home: FC<HomeProps> = ({ searchParams }) => {
   const { userName, token } = searchParams
-
-  if (!userName || !token) {
-    return (
-      <div className="flex flex-col justify-center align-center h-screen">
-        <GlobeError/>
-      </div>
-    )
-  }
 
   return <Main userName={userName} token={token}/>
 }
