164d0ceb6b766f95041890a108352195b0404d2f
unknown <chenyuanfei@genius.com>
2025-03-28 11:07:14 +0800

    Signed-off-by: unknown

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import type { AppInfo } from '@/types/app'
 
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
-export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+export const API_URL = `${process.env.NEXT_PUBLIC_API_URL || "https://genrui.577311.xyz/v1"}`
 
 export const APP_INFO: AppInfo = {
   title: 'Genrui AI Chat',
