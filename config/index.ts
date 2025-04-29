304e811f1187dc0e6248d7e874d47df6f3c4236b
unknown <chenyuanfei@genius.com>
2025-04-29 17:19:02 +0800

    Signed-off-by: 55

--- config/index.ts
+++ config/index.ts
@@ -10,7 +10,7 @@ export const APP_INFO: AppInfo = {
   default_language: 'en',
 }
 
-export const isShowPrompt = false
+export const isShowPrompt = true
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
