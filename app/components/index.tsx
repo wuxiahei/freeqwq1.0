eb3bb02cb5ee548dbcd7b82d923b5166e54ca10e
unknown <chenyuanfei@genius.com>
2025-04-29 10:43:01 +0800

    Signed-off-by: unknown

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -320,7 +320,8 @@ const Main: FC<IMainProps> = () => {
 
         setNewConversationInfo({
           name: t("app.chat.newChatDefaultName"),
-          introduction, suggested_questions,
+          introduction,
+          'suggested_questions': suggested_questions,
         });
         const prompt_variables =
           userInputsFormToPromptVariables(user_input_form);
