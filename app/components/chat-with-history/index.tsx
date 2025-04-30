fd2ce56a68cf553c83c1964151cc9815e6139fe0
unknown <chenyuanfei@genius.com>
2025-04-30 15:17:14 +0800

    Signed-off-by: ELY

--- app/components/chat-with-history/index.tsx
+++ app/components/chat-with-history/index.tsx
@@ -65,7 +65,7 @@ const ChatWithHistory: FC<ChatWithHistoryProps> = ({
       if (customConfig)
         document.title = `${site.title}`
       else
-        document.title = `${site.title} - Powered by Dify`
+        document.title = `${site.title}`
     }
   }, [site, customConfig, themeBuilder])
 
