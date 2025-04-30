87cc609dfa218ee39e67c399347e21369f1e358f
unknown <chenyuanfei@genius.com>
2025-04-30 10:47:44 +0800

    Signed-off-by: ELY

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
     return [
       {
         source: '/files/:path*',
-        destination: `${process.env.NEXT_PUBLIC_FILES_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ]
   },
