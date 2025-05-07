86455adaa669c8f4165cfcca5b165bffe4489371
unknown <chenyuanfei@genius.com>
2025-05-07 18:10:06 +0800

    Signed-off-by: ely

--- service/index.ts
+++ service/index.ts
@@ -62,7 +62,7 @@ export const updateFeedback = async ({ url, body }: { url: string; body: Feedbac
 }
 
 export const generationConversationName = async (id: string) => {
-  return post(`conversations/${id}/name`, { body: { auto_generate: true } })
+  return post(`conversations/${id}/name`, { body: { auto_generate: false } })
 }
 
 export const uploadRemoteFileInfo = (url: string) => {
