4df4b51a892a160230ca909b9e18dc18170861b6
unknown <chenyuanfei@genius.com>
2025-05-07 16:37:05 +0800

    Signed-off-by: ely

--- service/index.ts
+++ service/index.ts
@@ -40,12 +40,12 @@ export const sendChatMessage = async (
   }, { onData, onCompleted, onThought, onFile, onError, getAbortController, onMessageEnd, onMessageReplace, onNodeStarted, onWorkflowStarted, onWorkflowFinished, onNodeFinished })
 }
 // 获取会话列表
-export const fetchConversations = async (limit = 100, last_id = null, user = 'abc-123') => {
-  return get('conversations', { params: { limit, last_id, user } })
+export const fetchConversations = async (limit = 100, first_id: '', user = 'abc-123') => {
+  return get('conversations', { params: { limit, first_id, user } })
 }
 
 // 获取特定会话的聊天记录
-export const fetchChatList = async ( conversationId: string, limit = 20, last_id = '') => {
+export const fetchChatList = async ( conversationId: string, limit = 20, last_id: '') => {
   return get('messages', {
     params: { conversation_id: conversationId, limit, last_id }
   })
