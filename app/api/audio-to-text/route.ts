8cdaff0817ae750015453b52fe38bbc47464a392
unknown <chenyuanfei@genius.com>
2025-05-14 09:34:22 +0800

    Signed-off-by: ely

--- app/api/audio-to-text/route.ts
+++ app/api/audio-to-text/route.ts
@@ -6,7 +6,12 @@ import { client, getInfo } from '@/app/api/utils/common'
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.formData()
-    const { user } = getInfo(request)
+    const userName = formData.get('name')?.toString() // 从表单数据中获取用户输入的名称
+    
+    const { user: defaultUser, sessionId } = getInfo(request)
+    // 如果用户提供了名称，则使用它；否则使用默认生成的用户标识符
+    const user = userName ? `user_${APP_ID}:${userName}` : defaultUser
+    
     formData.append('user', user)
     const { data } = await client.aduioToText(formData)
     return NextResponse.json(data)
